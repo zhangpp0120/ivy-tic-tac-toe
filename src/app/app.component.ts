fce7d1cf7f5efb53e3b2a343efe26b9fb9e391df
zhangpp0120 <zhangpp0120@gmail.com>
2022-11-27 18:52:39 -0600

    cellClicked checkWin or Tie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,55 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name = 'Angular ' + VERSION.major;
-  row1 = ['o', 'x', 'o'];
-  row2 = ['x', 'o', 'x'];
-  row3 = ['o', 'o', 'o'];
   cells = [];
+  count = 0; // oxox
+  winner = '';
+  winnerMessage = '';
+  isGameEnd = false;
 
   ngOnInit() {
     this.newGame();
   }
   newGame() {
-    this.cells = [["", "", ""],["", "", ""],["", "", ""]];
+    this.cells = [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
+    ];
+  }
+  cellClicked(i, j) {
+    if (this.isGameEnd) {
+      this.reset();
+      return;
+    }
+    if (this.cells && this.cells[i][j] == '') {
+      if (this.count % 2 == 1) {
+        this.cells[i][j] = 'X';
+      } else {
+        this.cells[i][j] = 'O';
+      }
+      this.count++;
+    }
+
+    if (this.count >= 5) {
+      if (this.checkIfWin()) {
+        this.winnerMessage = this.winner + 'Win!';
+        this.isGameEnd = true;
+      } else if (this.count == 9) {
+        this.winnerMessage = 'Nobody Wins!';
+        this.isGameEnd = true;
+      }
+    }
+  }
+
+  checkIfWin(): boolean {
+    return false;
+  }
+  reset() {
+    this.newGame();
+    this.count = 0;
+    this.winner = '';
+    this.winnerMessage = '';
+    this.isGameEnd = false;
   }
 }
